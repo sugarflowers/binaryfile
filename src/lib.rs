c7d6f6b95640cf3437cb6ce1a5e1029398a14540
sugarflowers <128127805+sugarflowers@users.noreply.github.com>
2023-04-26 09:14:31 +0900

    add read function

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,7 @@
-/*
- * This file is part of PROJECT.
- *
- * PROJECT is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * PROJECT is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with PROJECT.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 use std::io::prelude::*;
 use std::fs::OpenOptions;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use std::io::{self, BufRead, BufReader, Read};
 
 pub struct BinaryWriter {
     pub filename: String,
@@ -73,6 +56,11 @@ impl BinaryReader {
             }
         )
     }
+    pub fn read(&mut self) -> Result<Vec<u8>, io::Error> {
+        let mut data = Vec::new();
+        self.reader.read_to_end(&mut data)?;
+        Ok(data)
+    }
 }
 
 impl Iterator for BinaryReader {
@@ -88,5 +76,11 @@ impl Iterator for BinaryReader {
             Err(e) => Some(Err(e)),
         }
     }
+}
+
+#[test]
+fn read_to_all() {
+    let br = BinaryReader::open("README.md").unwrap().read();
+    println!("{:?}", br);
 }
 
